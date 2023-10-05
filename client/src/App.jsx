e2612c8beb60f0fe172729b396c608eacb85a869
Lovely Sharma <lovelysingeras297@gmail.com>
2023-10-05 19:45:40 +0530

    Tested call to local server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+      .then(result => result.json())
+      .then(data => {
+        setMessage(data)
+        console.log(data);
+      }).catch(error => console.log(error))
+  }, []);
+
   return (
     <>
       <h1>Quiz Application</h1>
+      <div>{message.message}</div>
     </>
   )
 }
