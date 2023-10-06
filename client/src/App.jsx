7540d9b2bbd75f468f78f0e57e36142382df14c3
Lovely Sharma <lovelysingeras297@gmail.com>
2023-10-06 23:51:39 +0530

    Added router andother file improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,21 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/Authentication/Login";
+import RegistrationPage from "./components/Authentication/Register";
+import LandingPage from './components/LandingPage/LandingPage'
+import TrainingModeLanding from "./components/TrainingMode/TrainingModeLanding";
 
 function App() {
-
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:3000/')
-      .then(result => result.json())
-      .then(data => {
-        setMessage(data)
-        console.log(data);
-      }).catch(error => console.log(error))
-  }, []);
-
   return (
     <>
-      <h1>Quiz Application</h1>
-      <div>{message.message}</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/training-test" element={<TrainingModeLanding />}></Route>
+        </Routes>
+      </Router>
+
     </>
   )
 }
